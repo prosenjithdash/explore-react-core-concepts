377094de27df8a208324aa74c3ab0e64fa373f93
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-03-07 23:43:31 +0600

    Component added done

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+// It's call components
+function App() {
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
